9dca25c6e11be7c42144ba5af9a108648241dfed
sandhya <svuksept2020@gmail.com>
2025-01-10 14:28:52 +0000

    feat: modified login buttton style to match Google sign in button

--- app/src/main/java/com/northcoders/banditandroid/MainActivity.java
+++ app/src/main/java/com/northcoders/banditandroid/MainActivity.java
@@ -1,12 +1,21 @@
 package com.northcoders.banditandroid;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.northcoders.banditandroid.service.BandMateApiService;
+import com.northcoders.banditandroid.service.RetrofitInstance;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +24,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BandMateApiService bandMateApiService = RetrofitInstance.getService();
+        Call<String> greeting = bandMateApiService.getGreeting();
+        greeting.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+               String hello =  response.body();
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(getApplicationContext(),t.getMessage(), Toast.LENGTH_LONG).show();
+            }
         });
+
     }
 }
