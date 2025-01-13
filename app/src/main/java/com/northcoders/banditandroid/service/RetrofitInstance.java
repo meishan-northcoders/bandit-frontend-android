0f1a00100bde5cb793dab3cdd341094a701ac89c
Vincent <va543567@gmail.com>
2025-01-13 20:09:58 +0000

    feat: changed base url into BuildConfig

--- app/src/main/java/com/northcoders/banditandroid/service/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/banditandroid/service/RetrofitInstance.java
@@ -1,11 +1,12 @@
 package com.northcoders.banditandroid.service;
+import com.northcoders.banditandroid.BuildConfig;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 public class RetrofitInstance {
     private static Retrofit retrofit = null;
-    private static final String BASE_URL ="http://10.0.2.2:8080/api/v1/";
 
     public static BandMateApiService getService(){
         OkHttpClient client =  new OkHttpClient.Builder()
@@ -14,7 +15,7 @@ public class RetrofitInstance {
                 .build();
 
         if(retrofit == null){
-            retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+            retrofit = new Retrofit.Builder().baseUrl(BuildConfig.BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .client(client)
                     .build();
