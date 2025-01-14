9660a657f6d78f8777760945bc013223908b5353
Vincent <va543567@gmail.com>
2025-01-14 18:07:32 +0000

    feat: implement firebase without login

--- app/src/main/java/com/northcoders/banditandroid/MainActivity.java
+++ app/src/main/java/com/northcoders/banditandroid/MainActivity.java
@@ -1,20 +1,36 @@
 package com.northcoders.banditandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.northcoders.banditandroid.service.BandMateApiService;
 import com.northcoders.banditandroid.service.RetrofitInstance;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -25,30 +41,128 @@ import retrofit2.Retrofit;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
 
+    private FirebaseAuth mAuth;
+
+    private Button mSigninButton;
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        BandMateApiService bandMateApiService = RetrofitInstance.getService();
-        Call<ResponseBody> greeting = bandMateApiService.getGreeting();
-        greeting.enqueue(new Callback<ResponseBody>() {
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+
+
+//        BandMateApiService bandMateApiService = RetrofitInstance.getService();
+//        Call<ResponseBody> greeting = bandMateApiService.getGreeting();
+//        greeting.enqueue(new Callback<ResponseBody>() {
+//            @Override
+//            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+//                try {
+//                    String responseContent = response.body().string();
+//                    Log.d(TAG, "onSuccess: " + responseContent);
+//                } catch (IOException e) {
+//                    Log.e(TAG, "Error reading response body", e);
+//                }
+//            }
+//            @Override
+//            public void onFailure(@NonNull Call<ResponseBody> call, Throwable t) {
+//                Log.d(TAG, "onFailure: " + t.getMessage());
+//                Toast.makeText(getApplicationContext(),t.getMessage(), Toast.LENGTH_LONG).show();
+//            }
+//        });
+
+        this.mSigninButton = findViewById(R.id.btnGoogleSignIn);
+        this.mSigninButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                try {
-                    String responseContent = response.body().string();
-                    Log.d(TAG, "onSuccess: " + responseContent);
-                } catch (IOException e) {
-                    Log.e(TAG, "Error reading response body", e);
-                }
-            }
-            @Override
-            public void onFailure(@NonNull Call<ResponseBody> call, Throwable t) {
-                Log.d(TAG, "onFailure: " + t.getMessage());
-                Toast.makeText(getApplicationContext(),t.getMessage(), Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                signIn();
             }
         });
 
+
     }
+
+    private void updateUI(FirebaseUser firebaseUser) {
+
+    }
+
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            Toast.makeText(this, "Sign in success", Toast.LENGTH_SHORT).show();
+
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+
+            Toast.makeText(this, "Sign in failed", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "error: ", response.getError());
+        }
+    }
+
+    private void signIn() {
+
+//        List<AuthUI.IdpConfig> providers = Arrays.asList(
+//                new AuthUI.IdpConfig.GoogleBuilder().build()
+//        );
+//
+//        // Create and launch sign-in intent
+//        Intent signInIntent = AuthUI.getInstance()
+//                .createSignInIntentBuilder()
+//                .setAvailableProviders(providers)
+//                .build();
+//
+//        signInLauncher.launch(signInIntent);
+
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInAnonymously:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            String uid = user.getUid();
+                            Log.d(TAG, "Firebase uid: " + uid);
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+
+    }
+
 }
