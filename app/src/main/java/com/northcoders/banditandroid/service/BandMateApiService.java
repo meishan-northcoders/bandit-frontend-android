d0c47f65870538e16c02de73e8b5abb7826e9dcc
samprice <samprice101@hotmail.co.uk>
2025-01-20 15:19:48 +0000

    feat: add messages POST endpoints to BandMateApiService interface

--- app/src/main/java/com/northcoders/banditandroid/service/BandMateApiService.java
+++ app/src/main/java/com/northcoders/banditandroid/service/BandMateApiService.java
@@ -1,13 +1,20 @@
 package com.northcoders.banditandroid.service;
 
+import com.northcoders.banditandroid.model.CorrespondentRequestDTO;
+import com.northcoders.banditandroid.model.MessageRequestDTO;
+import com.northcoders.banditandroid.model.MessageResponseDTO;
 import com.northcoders.banditandroid.model.Profile;
 
 import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.HTTP;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface BandMateApiService {
     @GET("api/p1/greeting")
@@ -15,4 +22,11 @@ public interface BandMateApiService {
 
     @GET("profile")
     public Call<Profile> getProfile();
+
+    //messages endpoints
+    @POST("api/v1/messages")
+    public Call<MessageResponseDTO> saveMessage(@Header("Authorization") String authHeader, @Body MessageRequestDTO messageRequestDTO);
+
+    @POST("api/v1/messages/user")
+    public Call<List<MessageResponseDTO>> getMessagesBetweenUsers(@Header("Authorization") String authHeader, @Body CorrespondentRequestDTO correspondentRequestDTO);
 }
